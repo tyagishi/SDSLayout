a7ae55cd179b7e4ad87956dae6f7c77a356b4625
Tomoaki Yagishita <tyagishi@gmail.com>
2024-10-02 09:10:48 +0900

    F - improve example

--- SDSLayoutExample/SDSLayoutExample/ContentView.swift
+++ SDSLayoutExample/SDSLayoutExample/ContentView.swift
@@ -12,30 +12,87 @@ struct ContentView: View {
     var body: some View {
         VStack {
             RelativeHStack(hSpacing: 0) {
-                HStack {
-                    Text("Hello, world!")
-                    Text("Hello, world!")
-                        .frame(maxWidth: .infinity)
+                Text("Hello, world!").font(.largeTitle)
+                .layoutValue(key: LayoutHRatio.self, value: .ratio(0.7))
+                .frame(maxWidth: .infinity, maxHeight: .infinity)
+                .border(.yellow)
+                .overlay {
+                    VStack {
+                        Spacer()
+                        Divider().frame(height: 15).foregroundStyle(.red)
+                            .overlay {
+                                Text("ratio(0.7)").fixedSize()
+                            }
+                        Spacer()
+                        Spacer()
+                        Spacer()
+                    }
                 }
-                .layoutValue(key: LayoutHRatio.self, value: .ratio(0.5))
-                .border(.red)
-                Text("Hello, world!")
-                    .frame(maxWidth: .infinity)
-                    .layoutValue(key: LayoutHRatio.self, value: .ratio(0.5))
-                    .border(.blue)
-            }
-            Color.yellow
-            RelativeVStack(vSpacing: 0) {
-                Text("Hello, world a little bit tall!").font(.largeTitle)
-                    //.frame(maxWidth: .infinity, alignment: .center)
-                    .layoutValue(key: LayoutVRatio.self, value: .ratio(0.5))
-                    .border(.red)
+                Color.white
+                    .layoutValue(key: LayoutHRatio.self, value: .fix(100))
                     .frame(maxWidth: .infinity, maxHeight: .infinity)
                     .border(.yellow)
+                    .overlay {
+                        VStack {
+                            Spacer()
+                            Divider().frame(height: 15).foregroundStyle(.red)
+                                .overlay {
+                                    Text("fix(100)").fixedSize()
+                                }
+                            Spacer()
+                            Spacer()
+                            Spacer()
+                        }
+                    }
                 Text("Hello, world!")
-                    //.frame(maxWidth: .infinity, alignment: .center)
+                    .layoutValue(key: LayoutHRatio.self, value: .ratio(0.3))
+                    .frame(maxWidth: .infinity, maxHeight: .infinity)
+                    .border(.yellow)
+                    .overlay {
+                        VStack {
+                            Spacer()
+                            Divider().frame(height: 15).foregroundStyle(.red)
+                                .overlay {
+                                    Text("ratio(0.3)").fixedSize()
+                                }
+                            Spacer()
+                            Spacer()
+                            Spacer()
+                        }
+                    }
+            }
+            RelativeVStack(vSpacing: 0) {
+                Text("Hello, world largeTitle!").font(.largeTitle)
                     .layoutValue(key: LayoutVRatio.self, value: .ratio(0.5))
-                    .border(.blue)
+                    .frame(maxWidth: .infinity, maxHeight: .infinity)
+                    .border(.yellow)
+                    .overlay {
+                        HStack {
+                            Spacer()
+                            Divider().frame(width: 15).foregroundStyle(.red)
+                                .overlay {
+                                    Text("ratio(0.5)").fixedSize()
+                                }
+                            Spacer()
+                            Spacer()
+                            Spacer()
+                        }
+                    }
+                Text("Hello, world body!")
+                    .layoutValue(key: LayoutVRatio.self, value: .ratio(0.5))
+                    .frame(maxWidth: .infinity, maxHeight: .infinity)
+                    .overlay {
+                        HStack {
+                            Spacer()
+                            Divider().frame(width: 15).foregroundStyle(.red)
+                                .overlay {
+                                    Text("ratio(0.5)").fixedSize()
+                                }
+                            Spacer()
+                            Spacer()
+                            Spacer()
+                        }
+                    }
             }.border(.green)
         }
         .padding()
